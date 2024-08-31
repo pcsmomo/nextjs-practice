5b748ec7a0518bd855c58a3e2ca9926852eb3991
Noah Kim <dongwoo_333@hanmail.net>
2024-08-31 19:49:21 +1000

    9. Create user

--- 01-auth-tutorial/actions/register.ts
+++ 01-auth-tutorial/actions/register.ts
@@ -1,7 +1,9 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
 
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -13,8 +15,30 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields!" };
   }
 
-  // wait for 2 seconds
-  await sleep(2000);
+  // register a new user in the database.
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-  return { success: "Email sent!" };
+  const existingUser = await db.user.findUnique({
+    where: { email },
+  });
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // wait for 2 seconds
+  // await sleep(2000);
+
+  // TODO: Send verification token email
+
+  return { success: "User created!" };
 };
