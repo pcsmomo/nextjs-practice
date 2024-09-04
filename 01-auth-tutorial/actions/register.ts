20c62afd369ca0ed34a3ad0ee5493962b2e98dcd
Noah Kim <dongwoo_333@hanmail.net>
2024-09-04 20:51:08 +1000

    chore: create common functions to get user from db

--- 01-auth-tutorial/actions/register.ts
+++ 01-auth-tutorial/actions/register.ts
@@ -4,6 +4,8 @@ import * as z from "zod";
 import bcrypt from "bcrypt";
 
 import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
+
 import { RegisterSchema } from "@/schemas";
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -19,9 +21,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const { email, password, name } = validatedFields.data;
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const existingUser = await db.user.findUnique({
-    where: { email },
-  });
+  const existingUser = await getUserByEmail(email);
 
   if (existingUser) {
     return { error: "Email already in use!" };
