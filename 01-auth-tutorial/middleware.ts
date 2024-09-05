faaf002bccbfc0b77a055e61aa59b805d0c30a34
Noah Kim <dongwoo_333@hanmail.net>
2024-09-06 08:52:08 +1000

    feat(auth): auth check and routing in middleware

--- 01-auth-tutorial/middleware.ts
+++ 01-auth-tutorial/middleware.ts
@@ -1,14 +1,39 @@
 import NextAuth from "next-auth";
 
 import authConfig from "@/auth.config";
-// import { auth } from "./auth";
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "@/routes";
 
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
+  const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
   console.log("ROUTE: ", req.nextUrl.pathname);
   console.log("IS LOGGEDINL: ", isLoggedIn);
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
 });
 
 // Optionally, don't invoke Middleware on some paths
